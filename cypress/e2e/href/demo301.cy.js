71a2d8dbde1d7c4b633a5b13ce137137d3b00ec2
Rija William RALITERA <rijawilliamralitera@macbook-pro-de-rija.home>
2023-11-22 17:56:22 +0100

    add HREF tests

---
+++ cypress/e2e/href/demo301.cy.js
@@ -0,0 +1,20 @@
+describe('Test des liens href', () => {
+  it('Vérifie que tous les liens href fonctionnent avec une réponse 200 OK', () => {
+    cy.visit('https://www.facebook.com/');
+
+    cy.get('a[href]').each((lien) => {
+      cy.request({
+        url: lien.prop('href'),
+        followRedirect: false,
+      }).then((response) => {
+        if (response.status === 301 || response.status === 302) {
+          cy.request(response.headers.location).then((redirectedResponse) => {
+            expect(redirectedResponse.status).to.eq(200);
+          });
+        } else {
+          expect(response.status).to.eq(200);
+        }
+      });
+    });
+  });
+});
